bfaa26cc2003291cf38b1b7f1869f7f51f1521cf
Cathy Augustin <135758000+HolbyKate@users.noreply.github.com>
2024-01-28 09:26:30 +0100

    Fix: try an another way to validate checker

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
+
+const args = process.argv[2]
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
-    console.log(err || data);
+fs.readFile(args, 'utf8', (err, data)=>{
+    if err {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
 });
