a160c0f983a186d494e5cc67c0269e80ca9e2415
Cathy Augustin <135758000+HolbyKate@users.noreply.github.com>
2024-01-28 09:40:57 +0100

    Fix: just try if installation right

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,17 @@
 #!/usr/bin/node
 
-const args = process.argv[2]
 const fs = require('fs');
-fs.readFile(args, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    } 
+
+if (process.argv.length !== 3) {
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
     console.log(data);
+  }
 });
