61fe71d4781e99a015a94c65192f122e38476aff
Cathy Augustin <135758000+HolbyKate@users.noreply.github.com>
2024-01-28 09:29:56 +0100

    fix indentation and console.error

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,8 +4,8 @@ const args = process.argv[2]
 const fs = require('fs');
 fs.readFile(args, 'utf8', (err, data)=>{
     if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
+        console.error(err);
+        return;
+    } 
+    console.log(data);
 });
