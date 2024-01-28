428173360d4f0c87f26bfdac5fe11f9b1e42ff08
Cathy Augustin <135758000+HolbyKate@users.noreply.github.com>
2024-01-28 09:28:16 +0100

    Fix: condition error with ()

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const args = process.argv[2]
 const fs = require('fs');
 fs.readFile(args, 'utf8', (err, data)=>{
-    if err {
+    if (err) {
         console.log(err);
     } else {
         console.log(data);
