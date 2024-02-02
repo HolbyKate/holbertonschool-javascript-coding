c0972440dc6bc771c72f646cf9df3f9ca1491bd4
HolbyKate <6804@holbertonstudents.com>
2024-02-02 14:39:32 +0000

    Script to read the database if available or not

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+// create a function named countStudents that reads a CSV file and computes the number of students in each field
+function countStudents(path) {
+    const fs = require('fs');
+    const data = fs.readFileSync(path, 'utf8');
+    console.log(data);
+    if (data === undefined) throw new Error('Cannot load the database');
+    else {
+        const NUMBER_OF_STUDENTS = data.split('\n').length;
+        const fields = data.split('\n')[0].split(',');
+        const NUMBER_OF_FIELDS = fields.length;
+        console.log('Number of students: ' + NUMBER_OF_STUDENTS);
+        console.log(`Number of in ${fields}: ${count}. List: ${names.join(', ')}`);
+    }
+}
